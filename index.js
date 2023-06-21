bde49c8f7d6d7630ae30f950dfc23d5fe658129e
MD Zillur Rahman <zillurdeu@gmail.com>
2023-06-21 11:08:03 +0200

    feat: setup server

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
-import Express from "express";
-const app = Express();
+import express from "express";
+
+import usersRouter from "./routes/users.js";
+import { AuthRouter } from "./routes/auth.route.js";
+const app = express();
+
+//Middlewares
+app.use(express.json());
+
+app.use("/api/v1/users", usersRouter);
+app.use("/api/v1/auth", AuthRouter);
 
 app.listen(5000, () => console.log("API Working"));
